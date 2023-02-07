9653f61bd2d7cdf816bf0e96c87c0e7f3ae3cf61
SerBor <xxxserborxxx@gmail.com>
2023-02-07 14:56:51 +0300

    fix: reworking the code

--- Caruena-Loeva Basis/Matrix.cs
+++ Caruena-Loeva Basis/Matrix.cs
@@ -11,120 +11,131 @@ namespace Caruena_Loeva_Basis
         /// <param name="cols">Число столбцов</param>
         /// <param name="minVal">Нижний предел диапазона случайных чисел</param>
         /// <param name="maxVal">Верхний предел диапазона случайных чисел</param>
-        /// <returns></returns>
         public static double[,] MatrixRandom(int rows, int cols, double minVal, double maxVal)
         {
-            var rnd = new Random(Guid.NewGuid().GetHashCode());
+            var rnd = new Random(DateTime.Now.Millisecond);
             var result = new double[rows, cols];
-            for (int i = 0; i < rows; i++)
-                for (int j = 0; j < cols; j++)
-                    result[i, j] = (maxVal - minVal) * rnd.NextDouble() + minVal;
+            
+            for (var i = 0; i < rows; i++)
+            for (var j = 0; j < cols; j++)
+                result[i, j] = (maxVal - minVal) * rnd.NextDouble() + minVal;
+            
             return result;
         }
+
         /// <summary>
         /// Переумножение матриц.
         /// </summary>
         /// <param name="matrixA">1-ая матрица</param>
         /// <param name="matrixB">2-ая матрица (на которую умножаем)</param>
-        /// <returns></returns>
         public static double[,] MatrixProduct(double[,] matrixA, double[,] matrixB)
         {
-            var aRows = matrixA.GetLength(0); var aCols = matrixA.GetLength(1);
-            var bRows = matrixB.GetLength(0); var bCols = matrixB.GetLength(1);
+            var aRows = matrixA.GetLength(0);
+            var aCols = matrixA.GetLength(1);
+            var bRows = matrixB.GetLength(0);
+            var bCols = matrixB.GetLength(1);
 
             if (aCols != bRows)
                 throw new Exception("Несоответствующие матрицы!");
 
             var result = new double[aRows, bCols];
-            for (int i = 0; i < aRows; i++)
-                for (int j = 0; j < bCols; j++)
-                    for (int k = 0; k < aCols; k++)
-                        result[i, j] += matrixA[i, k] * matrixB[k, j];
+            for (var i = 0; i < aRows; i++)
+            for (var j = 0; j < bCols; j++)
+            for (var k = 0; k < aCols; k++)
+                result[i, j] += matrixA[i, k] * matrixB[k, j];
 
             return result;
         }
+
         /// <summary>
         /// Транспонирование матрицы.
         /// </summary>
         /// <param name="matrix">Матрица</param>
-        /// <returns></returns>
         public static double[,] MatrixTransposition(double[,] matrix)
         {
-            var M = matrix.GetLength(0);    //Число строк
-            var N = matrix.GetLength(1);    //Число столбцов
-            var matrixT = new double[N, M];
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < M; j++)
-                    matrixT[i, j] = matrix[j, i];
+            var m = matrix.GetLength(0);
+            var n = matrix.GetLength(1);
+            var matrixT = new double[n, m];
+            
+            for (var i = 0; i < n; i++)
+            for (var j = 0; j < m; j++)
+                matrixT[i, j] = matrix[j, i];
 
             return matrixT;
         }
+
         /// <summary>
         /// Вычисление обратной сингулярной матрицы с применением регуляризации.
         /// </summary>
         /// <param name="singularityMatrix">Сингулярная матрица</param>
-        /// <returns></returns>
         public static double[,] InverseSingularityMatrix(double[,] singularityMatrix)
         {
-            var M = singularityMatrix.GetLength(0); //Число строк
-            var N = singularityMatrix.GetLength(1); //Число столбцов
+            var m = singularityMatrix.GetLength(0);
+            var n = singularityMatrix.GetLength(1);
 
-            var singMaxValue = 0.0;
-            for (int i = 0; i < ((M > N) ? N : M); i++)
-                if (singularityMatrix[i, i] > singMaxValue) singMaxValue = singularityMatrix[i, i];
+            var singMaxValue = double.MinValue;
+            for (var i = 0; i < (m > n ? n : m); i++)
+                singMaxValue = Math.Max(singularityMatrix[i, i], singMaxValue);
 
-            var invSingularityMatrix = new double[N, M];
-            for (int i = 0; i < N; i++)
-                for (int j = 0; j < M; j++)
-                    invSingularityMatrix[i, j] = (i == j) ? ((singularityMatrix[i, i] > singMaxValue * 0.01f) ? 1 / singularityMatrix[i, i] : 0) : 0;
+            var invSingularityMatrix = new double[n, m];
+            for (var i = 0; i < n; i++)
+            for (var j = 0; j < m; j++)
+                invSingularityMatrix[i, j] = (i == j) ? (singularityMatrix[i, i] > singMaxValue * 0.01f) ? 1 / singularityMatrix[i, i] : 0 : 0;
 
             return invSingularityMatrix;
         }
+
         /// <summary>
         /// Сингулярное разложение.
         /// </summary>
-        /// <param name="A">Матрица, которую требуется разложить</param>
-        /// <param name="U">Левая сингулярная матрица</param>
-        /// <param name="VT">Правая сингулярная матрица</param>
+        /// <param name="mtxA">Матрица, которую требуется разложить</param>
+        /// <param name="mtxU">Левая сингулярная матрица</param>
+        /// <param name="mtxV">Правая сингулярная матрица</param>
         /// <returns>Сингулярная матрица</returns>
-        public static double[,] MatrixSVD(double[,] A, out double[,] U, out double[,] V)
+        public static double[,] SVD(double[,] mtxA, out double[,] mtxU, out double[,] mtxV)
         {
-            var M = A.GetLength(0);     //Число строк
-            var N = A.GetLength(1);     //Число столбцов
+            var m = mtxA.GetLength(0);
+            var n = mtxA.GetLength(1);
 
-            double[] W;
+            double[] w;
+            
             // Библиотека alglib https://www.alglib.net/docs.php
-            alglib.rmatrixsvd(A, M, N, 2, 2, 2, out W, out U, out V);
+            alglib.rmatrixsvd(mtxA, m, n, 2, 2, 2, out w, out mtxU, out mtxV);
 
-            V = MatrixTransposition(V);
-            var matrixSing = new double[M, N];
-            for (int i = 0; i < M; i++)
-                for (int j = 0; j < N; j++)
-                    matrixSing[i, j] = (i == j) ? W[i] : 0;
+            mtxV = MatrixTransposition(mtxV);
+            var matrixSing = new double[m, n];
+            
+            for (var i = 0; i < m; i++)
+            for (var j = 0; j < n; j++)
+                matrixSing[i, j] = (i == j) ? w[i] : 0;
 
             return matrixSing;
         }
+
         /// <summary>
         /// Вычисление невязки СЛУ.
         /// </summary>
-        /// <param name="A"></param>
+        /// <param name="mtxA"></param>
         /// <param name="x"></param>
         /// <param name="b"></param>
         /// <returns></returns>
-        public static double Discrepancy(double[,] A, double[,] x, double[,] b)
+        public static double Discrepancy(double[,] mtxA, double[,] x, double[,] b)
         {
-            var M = A.GetLength(0);     //Число строк
-            var N = A.GetLength(1);     //Число столбцов
+            var m = mtxA.GetLength(0);
+            var n = mtxA.GetLength(1);
 
-            var d = 0.0;
-            for (int i = 0; i < M; i++) {
+            var d = 0d;
+            for (var i = 0; i < m; i++)
+            {
                 var sum = 0.0;
-                for (int j = 0; j < N; j++)
-                    sum += A[i, j] * x[j, 0];
+                for (var j = 0; j < n; j++)
+                    sum += mtxA[i, j] * x[j, 0];
                 d += (b[i, 0] - sum) * (b[i, 0] - sum);
             }
-            return d / M;
+
+            return d / m;
         }
+
         /// <summary>
         /// Вычисление АКП сигнала.
         /// </summary>
@@ -132,26 +143,25 @@ namespace Caruena_Loeva_Basis
         /// <param name="x">Сигнал х</param>
         private static double AutoCorrelationSequence(int m, double[] x)
         {
-            var sum = 0.0;
+            var sum = 0d;
             m = Math.Abs(m);
-            for (int k = 0; k < x.Length - m; k++)
+            for (var k = 0; k < x.Length - m; k++)
                 sum += x[k] * x[k + m];
             return sum / (x.Length - m);
         }
+
         /// <summary>
         /// Вычисление АКМ сигнала.
         /// </summary>
         /// <param name="x">Сигнал х</param>
-        /// <param name="y">Сигнад y</param>
         /// <param name="p"></param>
-        /// <returns></returns>
         public static double[,] AutoCorrelationMatrix(double[] x, int p)
         {
-            var R = new double[p, p];
-            for (int i = 0; i < p; i++)
-                for (int j = 0; j < p; j++)
-                    R[i, j] = AutoCorrelationSequence(i - j, x);
-            return R;
+            var result = new double[p, p];
+            for (var i = 0; i < p; i++)
+            for (var j = 0; j < p; j++)
+                result[i, j] = AutoCorrelationSequence(i - j, x);
+            return result;
         }
     }
 }
--- Caruena-Loeva Basis/Signals.cs
+++ Caruena-Loeva Basis/Signals.cs
@@ -2,9 +2,8 @@
 
 namespace Caruena_Loeva_Basis
 {
-    class Signal
+    class Signals
     {
-        private static readonly double DoublePI = 2 * Math.PI;
         /// <summary>
         /// Гармоническая функция.
         /// </summary>
@@ -12,21 +11,17 @@ namespace Caruena_Loeva_Basis
         /// <param name="f">Частота</param>
         /// <param name="phi">Начальная фаза</param>
         /// <param name="t">Переменная времени</param>
-        private static double HarmonicFunction(double A, double f, double phi, double t)
-        {
-            return A * Math.Sin(DoublePI * f * t + phi);
-        }
+        private static double HarmonicFunction(double a, double f, double phi, double t) => a * Math.Sin(2 * Math.PI * f * t + phi);
+
         /// <summary>
         /// Гауссова функиция.
         /// </summary>
         /// <param name="a">Амплитуда</param>
-        /// <param name="SKO">Среднеквдаратичное отклонение</param>
-        /// <param name="E">Математическое ожидание</param>
+        /// <param name="s">Дисперсия</param>
+        /// <param name="shift">Сдвиг</param>
         /// <param name="t">Переменная времени</param>
-        private static double GaussFunction(double A, double SKO, double E, double t)
-        {
-            return A * Math.Exp(-Math.Pow((t - E) / SKO, 2));
-        }
+        private static double GaussFunction(double a, double s, double shift, double t) => a * Math.Exp(-Math.Pow((t - shift) / s, 2));
+
         /// <summary>
         /// Экспоненциально-затухающая функция.
         /// </summary>
@@ -34,46 +29,48 @@ namespace Caruena_Loeva_Basis
         /// <param name="f">Частота</param>
         /// <param name="phase">Фаза</param>
         /// <param name="t">Переменная времени</param>
-        /// <returns></returns>
-        private static double ExponentialDecayingFunction(double a, double f, double phase, double t)
-        {
-            return Math.Exp(-a * t) * Math.Sin(DoublePI * f * t + phase);
-        }
+        private static double ExponentialDecayingFunction(double a, double f, double phase, double t) => Math.Exp(-a * t) * Math.Sin(2 * Math.PI * f * t + phase);
 
         /// <summary>
         /// Генерация полигармонического сигнала.
         /// </summary>
         /// <param name="length">Число отсчётов</param>
-        /// <param name="A">Амплитуды</param>
+        /// <param name="a">Амплитуды</param>
         /// <param name="f">Частоты</param>
         /// <param name="phi">Фазы</param>
         /// <param name="dt">Частота дискретизации</param>
         /// <returns>Дискретный полигармонический сигнал</returns>
-        public static double[] GenerateHarmonicSignal(int length, double[] A, double[] f, double[] phi, double dt)
+        public static double[] GenerateHarmonicSignal(int length, double[] a, double[] f, double[] phi, double dt)
         {
-            var signal = new double[length];
-            for (int i = 0; i < length; i++)
-                for (int k = 0; k < A.Length; k++)
-                    signal[i] += HarmonicFunction(A[k], f[k], phi[k], i * dt);
-            return signal;
+            var result = new double[length];
+            
+            for (var i = 0; i < length; i++)
+            for (var k = 0; k < a.Length; k++)
+                result[i] += HarmonicFunction(a[k], f[k], phi[k], i * dt);
+            
+            return result;
         }
+
         /// <summary>
         /// Генерация гауссова сигнала.
         /// </summary>
         /// <param name="length">Число отсчётов</param>
-        /// <param name="A">Амплитуды</param>
-        /// <param name="SKO">Среднеквдаратичные отклонения</param>
-        /// <param name="E">Математические ожидания</param>
+        /// <param name="a">Амплитуды</param>
+        /// <param name="s">Дисперсии</param>
+        /// <param name="shift">Сдвиги</param>
         /// <param name="dt">Частота дискретизации</param>
         /// <returns>Дискретный гауссов сигнал</returns>
-        public static double[] GenerateGaussSignal(int length, double[] A, double[] SKO, double[] E, double dt)
+        public static double[] GenerateGaussSignal(int length, double[] a, double[] s, double[] shift, double dt)
         {
-            var signal = new double[length];
-            for (int i = 0; i < length; i++)
-                for (int k = 0; k < A.Length; k++)
-                    signal[i] += GaussFunction(A[k], SKO[k], E[k], i * dt);
-            return signal;
+            var result = new double[length];
+            
+            for (var i = 0; i < length; i++)
+            for (var k = 0; k < a.Length; k++)
+                result[i] += GaussFunction(a[k], s[k], shift[k], i * dt);
+            
+            return result;
         }
+
         /// <summary>
         /// Генерация полигармонического затухающего сигнала.
         /// </summary>
@@ -85,11 +82,13 @@ namespace Caruena_Loeva_Basis
         /// <returns>Дискретный полигармонический затухающий сигнал</returns>
         public static double[] GenerateExponentialDecayingSignal(int length, double[] a, double[] f, double[] phi, double dt)
         {
-            var signal = new double[length];
-            for (int i = 0; i < length; i++)
-                for (int k = 0; k < a.Length; k++)
-                    signal[i] += ExponentialDecayingFunction(a[k], f[k], phi[k], i * dt);
-            return signal;
+            var result = new double[length];
+            
+            for (var i = 0; i < length; i++)
+            for (var k = 0; k < a.Length; k++)
+                result[i] += ExponentialDecayingFunction(a[k], f[k], phi[k], i * dt);
+            
+            return result;
         }
     }
 }
